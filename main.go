c4eae0f90538fe78c4616285c8715eacc760a00e
Mostain Billah <bill.rassel@gmail.com>
2022-09-12 11:55:40 +0600

    test with main.go

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"mastercode/lexer"
+	"mastercode/token"
+)
+
 func main() {
 
+	input := "x=a+(b*c){,};"
+	
+	lex := lexer.New(input)
+	for {
+		tok := lex.NextToken()
+		if tok.Type == token.EOF {
+			break
+		}
+		fmt.Println(tok.Type, tok.Literal)
+	}
 }
