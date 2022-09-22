eac2392c6e912c846bea3aba4498bce4b8da3544
Mostain Billah <bill.rassel@gmail.com>
2022-09-22 20:17:23 +0600

    Boolean object refactored

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"mastercode/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 
 	switch node := node.(type) {
@@ -15,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 
 	//Expression
 	case *ast.IntegerLiteral:
@@ -30,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
