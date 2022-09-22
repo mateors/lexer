ddc4ca1ec4d7f0b62959b0a0d7e1bc36a689f794
Mostain Billah <bill.rassel@gmail.com>
2022-09-22 20:06:07 +0600

    REPL updated

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"mastercode/evaluator"
 	"mastercode/lexer"
 	"mastercode/parser"
 )
@@ -38,8 +39,14 @@ func Start(input io.Reader, output io.Writer) {
 			continue
 		}
 
-		io.WriteString(output, program.String())
-		io.WriteString(output, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(output, evaluated.Inspect())
+			io.WriteString(output, "\n")
+		}
+
+		// io.WriteString(output, program.String())
+		// io.WriteString(output, "\n")
 
 		// for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
 		// 	fmt.Fprintf(output, "%v %s\n", tok.Type, tok.Literal)
