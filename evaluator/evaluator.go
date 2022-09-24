eff44d30deaa8aeecdaf19de8966006a502c6ad2
Mostain Billah <bill.rassel@gmail.com>
2022-09-24 07:27:36 +0600

    builtin function test passes and working

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -274,11 +274,19 @@ func newError(format string, a ...interface{}) *object.Error {
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+
+	// val, ok := env.Get(node.Value)
+	// if !ok {
+	// 	return newError("identifier not found: " + node.Value)
+	// }
+	// return val
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
@@ -295,13 +303,27 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+
+	// function, ok := fn.(*object.Function)
+	// if !ok {
+	// 	return newError("not a function: %s", fn.Type())
+	// }
+	// extendedEnv := extendFunctionEnv(function, args)
+	// evaluated := Eval(function.Body, extendedEnv)
+	// return unwrapReturnValue(evaluated)
+	switch fn := fn.(type) {
+
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+
+	case *object.Builtin:
+		return fn.Fn(args...)
+
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
